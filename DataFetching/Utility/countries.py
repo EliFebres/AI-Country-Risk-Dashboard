5c844151d4de3026f9a998e425546f44263ad645
Eli Febres Jr <eli.febresjr@gmail.com>
2025-07-27 16:56:50 -0400

    Initial Commit & Initial List of Countries

---
+++ DataFetching/Utility/countries.py
@@ -0,0 +1,16 @@
+# Manual List of Countries to Simplify Program 
+# Keeping to >= 50 Countries due to cost: ~$75 a month (5,000 articles a month limit (100 articles per country))
+
+developed_markets = [
+    "Australia", "Austria", "Belgium", "Canada", "Denmark", "Finland", "France",
+    "Germany", "Hong Kong", "Ireland", "Israel", "Italy", "Japan", "Netherlands",
+    "New Zealand", "Norway", "Portugal", "Singapore", "Spain", "Sweden",
+    "Switzerland", "United Kingdom", "United States"
+]
+
+emerging_markets = [
+    "Argentina", "Brazil", "Chile", "China", "Colombia", "Czech Republic", "Egypt",
+    "Greece", "Hungary", "India", "Indonesia", "Kuwait", "Malaysia", "Mexico",
+    "Pakistan", "Peru", "Philippines", "Poland", "Qatar", "Saudi Arabia",
+    "South Africa", "South Korea", "Taiwan", "Thailand", "Turkey", "United Arab Emirates"
+]
